9bfbd54a93774d5edf051552d0d796bb1c770e23
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2023-01-28 17:14:42 +0530

    Custom Store implementation for creating new todo in localstorage. Closes #11

--- src/stores/todos.store.ts
+++ src/stores/todos.store.ts
@@ -1,14 +1,18 @@
-import { fetchTodosFromDb } from './../../backend/backend';
+import type { CreateTodo } from './../../models/dtos/create-todo.dto';
+import { fetchTodosFromDb, createNewTodo } from './../../backend/backend';
 import { writable } from 'svelte/store';
 
 const createTodoStore = () => {
   const todos = fetchTodosFromDb();
 
-
   const { subscribe, set, update } = writable(todos);
 
   return {
     subscribe,
+    createTodo: (createTodo: CreateTodo) => {
+      const updatedTodos = createNewTodo(createTodo);
+      set(updatedTodos);
+    },
   };
 };
 
