e56aad0026af6b4f0dc75dfdea67fdc6d180f40c
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2023-01-28 16:28:28 +0530

    DTO Implementation for updating todo in localstorage. Closes #5

---
+++ models/dtos/update-todo.dto.ts
@@ -0,0 +1,16 @@
+export class UpdateTodo {
+  public uuid: string;
+  public content: string;
+  public isCompleted: boolean;
+
+  static newDto(uuid: string, content: string, isCompleted): UpdateTodo {
+    const newDto = new UpdateTodo();
+
+    newDto.uuid = uuid;
+    newDto.content = content;
+    newDto.isCompleted = isCompleted;
+
+    return newDto;
+  }
+}
+
