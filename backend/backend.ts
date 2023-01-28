a1548b232af409e93c66dcaafd7578ca409902b8
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2023-01-28 16:18:53 +0530

    Function Implementation for creating new todo in localstorage. Closes #4

--- backend/backend.ts
+++ backend/backend.ts
@@ -1,3 +1,4 @@
+import { CreateTodo } from './../models/dtos/create-todo.dto';
 import { Todo } from '../models/todo.model';
 import { BACKEND_KEY } from './../constants/backend';
 
@@ -5,11 +6,21 @@ export const fetchTodosFromDb = (): Todo[] => {
   const backendTodosString = localStorage.getItem(BACKEND_KEY);
 
   if (backendTodosString) {
-    const todos: any[] = JSON.parse(backendTodosString).todos;
+    const todos: any[] = JSON.parse(backendTodosString);
 
     return todos.map((rawTodo: any) => Todo.fromJson(rawTodo));
   } else {
     return [];
   }
+};
+
+export const createNewTodo = (createTodo: CreateTodo): Todo[] => {
+  const newTodo = Todo.newTodo(createTodo.content);
+  const todos = fetchTodosFromDb();
+
+  todos.push(newTodo);
+  localStorage.setItem(BACKEND_KEY, JSON.stringify(todos));
+
+  return fetchTodosFromDb();
 };
 
