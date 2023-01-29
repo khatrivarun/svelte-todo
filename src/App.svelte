6f5a67c6ded2c365119e3ccd8e8194753bed480b
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2023-01-29 16:16:37 +0530

    UI Implementation for Create Todo Form. Closes #15

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
+  import TodoForm from './components/TodoForm.svelte';
+  import { todoStore } from './stores/todos.store';
+
   let name = 'world';
 </script>
 
 <div class="dark">
-  <h1 class="text-3xl font-bold">Hello, {name}</h1>
+  <h1>SVELTE TODOS</h1>
+  <TodoForm bind:value={name} />
+  <p>{$todoStore.length}</p>
 </div>
---
+++ src/components/TodoForm.svelte
@@ -0,0 +1,29 @@
+<script>
+  import { todoStore } from './../stores/todos.store';
+  export let value = '';
+
+  function onSubmit() {
+    todoStore.createTodo({
+      content: value,
+    });
+
+    value = '';
+  }
+</script>
+
+<div class="flex flex-col justify-center items-center">
+  <div class="bg-slate-200 shadow-lg rounded">
+    <form on:submit|preventDefault={onSubmit}>
+      <div class="flex flex-row justify-center items-center m-4">
+        <input
+          type="text"
+          name="content"
+          id="content"
+          class="rounded-md p-1 m-1 mx-5"
+          bind:value
+        />
+        <input type="submit" class="px-5 p-1 bg-slate-300 rounded" />
+      </div>
+    </form>
+  </div>
+</div>
